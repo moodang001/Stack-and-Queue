9658961fc6d7c8404a81eced572cd5721bf757dd
moodang001 <moo_dang_001@hotmail.com>
2018-01-17 15:46:25 +0700

    Lab

---
+++ src/test/java/rmuti/ArrayListStack/TestStack.java
@@ -0,0 +1,16 @@
+package rmuti.ArrayListStack;
+
+public class TestStack {
+	
+	public static void main(String[] args) {
+		ArrayListStack stack = new ArrayListStack();
+		stack.push("macos");
+		stack.push("linux");
+		stack.push("windows");
+		System.out.println(stack);
+		
+		String data = (String)stack.pop();
+		System.out.println(stack);
+	}
+
+}
